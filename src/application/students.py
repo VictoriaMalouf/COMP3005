c81db72d013af1df94be4371bba675bbb000aec0
Victoria Malouf <victoriamalouf@cmail.carleton.ca>
2024-03-12 17:25:02 -0400

    added argparse

--- src/application/students.py
+++ src/application/students.py
@@ -1,3 +1,4 @@
+import argparse
 import asyncio
 import psycopg
 
@@ -21,9 +22,6 @@ async def getAllStudents():
 async def addStudent(first_name:str, last_name:str, email: str, enrollment_date: str): 
     """
     Inserts a new student record into the students table 
-
-    INSERT INTO students (first_name, last_name, email, enrollment_date) 
-    VALUES (first_name, last_name, email, enrollment_date)
     """
     async with await psycopg.AsyncConnection.connect(dbname=dbname, user=user, password=password, host=host, port=port) as conn:
         async with conn.cursor() as cursor:
@@ -45,19 +43,43 @@ async def deleteStudent(student_id: int):
         async with conn.cursor() as cursor:
             await cursor.execute(f"DELETE FROM students WHERE student_id={student_id}")
 
+def main():
+    parser = argparse.ArgumentParser(description='students table - CRUD operations')
+
+    parser.add_argument('--get','-g', action='store_true', help='Retrieves and displays all records from the students table')
+    parser.add_argument('--add','-a', nargs=4, metavar=('first_name', 'last_name', 'email', 'enrollment_date'), help='Inserts a new student record into the students table given the first name, last name, email, and enrollement date')
+    parser.add_argument('--update','-u',nargs=2, metavar=('student_id', 'email'), help='Updates the email address for a student with the specified student_id')
+    parser.add_argument('--delete','-d', metavar='student_id', type=int, help='Deletes the record of the student with the specified student_id')
+
+    args = parser.parse_args()
+
+    if args.get:
+        asyncio.run(getAllStudents())
+    elif args.add:
+        first_name, last_name, email, enrollment_date = args.add
+        asyncio.run(addStudent(first_name, last_name, email, enrollment_date))
+    elif args.update:
+        id, email = args.update
+        asyncio.run(updateStudentEmail(id, email))
+    elif args.delete:
+        #id = args.delete
+        asyncio.run(deleteStudent(args.delete))
+
 if __name__ == "__main__":
-    print("getAllStudents:\n")
-    asyncio.run(getAllStudents())
-    print()
-    print("addStudent:\n")
-    asyncio.run(addStudent('Victoria', 'Malouf', 'victoriamalouf@cmail.carleton.ca', '2024-03-09'))
-    asyncio.run(getAllStudents())
-    print()
-    print("updateStudentEmail:\n")
-    asyncio.run(updateStudentEmail(4, 'victoriam@gmail.com'))
-    asyncio.run(getAllStudents())
-    print()
-    print("deleteStudent:\n")
-    asyncio.run(deleteStudent(4))
-    asyncio.run(getAllStudents())
+    main()
+    # print("getAllStudents:\n")
+    # asyncio.run(getAllStudents())
+    # print()
+    # print("addStudent:\n")
+    # asyncio.run(addStudent('Victoria', 'Malouf', 'victoriamalouf@cmail.carleton.ca', '2024-03-09'))
+    # asyncio.run(getAllStudents())
+    # print()
+    # print("updateStudentEmail:\n")
+    # asyncio.run(updateStudentEmail(4, 'victoriam@gmail.com'))
+    # asyncio.run(getAllStudents())
+    # print()
+    # print("deleteStudent:\n")
+    # asyncio.run(deleteStudent(4))
+    # asyncio.run(getAllStudents())
+    # asyncio.run(getAllStudents())
     
