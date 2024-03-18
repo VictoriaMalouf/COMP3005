0a4a1e5874bf032f5d0f1ffc61740c21803f3d62
Victoria Malouf <victoriamalouf@cmail.carleton.ca>
2024-03-17 23:59:56 -0400

    data validation

--- src/application/students.py
+++ src/application/students.py
@@ -33,25 +33,35 @@ async def updateStudentEmail(student_id: int, new_email: str):
     """
     Updates the email address for a student with the specified student_id
     """
-    async with await psycopg.AsyncConnection.connect(dbname=dbname, user=user, password=password, host=host, port=port) as conn:
-        async with conn.cursor() as cursor:
-            await cursor.execute(f"UPDATE students SET email='{new_email}' WHERE student_id={student_id}")
+    try: 
+        int(student_id)
+        async with await psycopg.AsyncConnection.connect(dbname=dbname, user=user, password=password, host=host, port=port) as conn:
+            async with conn.cursor() as cursor:
+                await cursor.execute(f"UPDATE students SET email='{new_email}' WHERE student_id={student_id}")
+    except:
+        print("Ensure you provide a proper student id (integer value)")
 
+    
 async def deleteStudent(student_id: int):
     """
     Deletes the record of the student with the specified student_id
     """
-    async with await psycopg.AsyncConnection.connect(dbname=dbname, user=user, password=password, host=host, port=port) as conn:
-        async with conn.cursor() as cursor:
-            await cursor.execute(f"DELETE FROM students WHERE student_id={student_id}")
+    try: 
+        int(student_id)
+        async with await psycopg.AsyncConnection.connect(dbname=dbname, user=user, password=password, host=host, port=port) as conn:
+            async with conn.cursor() as cursor:
+                await cursor.execute(f"DELETE FROM students WHERE student_id={student_id}")
+    except:
+        print("Ensure you provide a proper student id (integer value)")
+    
 
 def main():
     parser = argparse.ArgumentParser(description='students table - CRUD operations')
 
-    parser.add_argument('--get','-g', action='store_true', help='Retrieves and displays all records from the students table')
-    parser.add_argument('--add','-a', nargs=4, metavar=('first_name', 'last_name', 'email', 'enrollment_date'), help='Inserts a new student record into the students table given the first name, last name, email, and enrollement date')
-    parser.add_argument('--update','-u',nargs=2, metavar=('student_id', 'email'), help='Updates the email address for a student with the specified student_id')
-    parser.add_argument('--delete','-d', metavar='student_id', type=int, help='Deletes the record of the student with the specified student_id')
+    parser.add_argument('--get', action='store_true', help='Retrieves and displays all records from the students table')
+    parser.add_argument('--add', nargs=4, metavar=('first_name', 'last_name', 'email', 'enrollment_date'), help='Inserts a new student record into the students table given the first name, last name, email, and enrollement date')
+    parser.add_argument('--update', nargs=2, metavar=('student_id', 'email'), help='Updates the email address for a student with the specified student_id')
+    parser.add_argument('--delete', metavar='student_id', type=int, help='Deletes the record of the student with the specified student_id')
 
     args = parser.parse_args()
 
